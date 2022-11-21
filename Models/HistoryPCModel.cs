8f9aa6e93d3d759a461fb2b107ce18a89f3780eb
Max <98024827+MaxGog@users.noreply.github.com>
2022-11-21 13:06:17 +0300

    Update README.md

---
+++ Models/HistoryPCModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+
+using SQLite;
+
+namespace PC_support.Models
+{
+    public class HistoryPC
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string type { get; set; }
+        public string model { get; set; }
+        public string СPU_model { get; set; }
+        public string image { get; set; }
+        public string ROM_str { get; set; }
+        public string OS_str { get; set; }
+        public string typeCPU { get; set; } 
+        public string VideoCard_model { get; set; }
+        public string typeRAM { get; set; }
+        public int RAM { get; set; }
+        public int ROM { get; set; }
+        public int kernels { get; set; }
+        public int VideoCard { get; set; }
+        private double CPU { get; set; }
+    }
+}
---
+++ ViewModels/SettingsViewModel.cs
@@ -0,0 +1,63 @@
+﻿using PC_support.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+
+namespace PC_support.ViewModels
+{
+    internal class SettingsViewModel
+    {
+        private ElementTheme _elementTheme = ThemeSelectorService.Theme;
+        public ElementTheme ElementTheme
+        {
+            get { return _elementTheme; }
+
+            set { SetProperty(ref _elementTheme, value); }
+        }
+        private ICommand _switchThemeCommand;
+        public ICommand SwitchThemeCommand
+        {
+            get
+            {
+                if (_switchThemeCommand == null)
+                {
+                    _switchThemeCommand = new RelayCommand<ElementTheme>(
+                        async (param) =>
+                        {
+                            ElementTheme = param;
+                            await ThemeSelectorService.SetThemeAsync(param);
+                        });
+                }
+
+                return _switchThemeCommand;
+            }
+        }
+        private string GetVersionDescription()
+        {
+            var appName = "AppDisplayName".GetLocalized();
+            var package = Package.Current;
+            var packageId = package.Id;
+            var version = packageId.Version;
+
+            return $"{appName} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        }
+
+        private string _versionDescription;
+        public string VersionDescription
+        {
+            get { return _versionDescription; }
+
+            set { SetProperty(ref _versionDescription, value); }
+        }
+        public Task InitializeAsync()
+        {
+            VersionDescription = GetVersionDescription();
+            return Task.CompletedTask;
+        }
+    }
+}
