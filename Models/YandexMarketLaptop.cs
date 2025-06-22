65c2b82aef2434a7201a37e6100bf7d48f178c08
MaxGog <max.gog2005@outlook.com>
2025-06-22 21:39:34 +0300

    Добавление парсинга Яндекс Маркета

---
+++ Models/YandexMarketLaptop.cs
@@ -0,0 +1,10 @@
+namespace ChoosingGadgets.Models;
+
+public class YandexMarketLaptop
+{
+    public string Title { get; set; }
+    public string Price { get; set; }
+    public string ImageUrl { get; set; }
+    public string ProductUrl { get; set; }
+    public string Rating { get; set; }
+}
