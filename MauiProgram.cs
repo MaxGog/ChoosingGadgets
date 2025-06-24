aa3e17f1dcb45bebb7a03bfa51111b55c61d786f
MaxGog <max.gog2005@outlook.com>
2025-06-24 15:21:35 +0300

    Структуризация регистрации сервисов

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,43 +2,62 @@
 using ChoosingGadgets.Services;
 using ChoosingGadgets.ViewModels;
 using ChoosingGadgets.Views;
-
 using Microsoft.Extensions.Logging;
 
 namespace ChoosingGadgets;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
 
-		builder.Services.AddSingleton<DeviceRepository>();
-        builder.Services.AddSingleton<ParserService>();
-        builder.Services.AddSingleton<BackgroundParserService>();
-        
-        builder.Services.AddSingleton<DevicesViewModel>();
-		builder.Services.AddSingleton<SettingsViewModel>();
-		builder.Services.AddTransient<ResultsViewModel>();
-		builder.Services.AddTransient<AddDeviceViewModel>();
+        RegisterServices(builder.Services);
 
-		builder.Services.AddSingleton<QuestionnairePage>();
-		builder.Services.AddSingleton<ResultsPage>();
-        builder.Services.AddSingleton<DevicesPage>();
-		builder.Services.AddSingleton<SettingsPage>();
-		builder.Services.AddTransient<AddDevicePage>();
+        RegisterViewModels(builder.Services);
+
+        RegisterPages(builder.Services);
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
+
+    private static void RegisterServices(IServiceCollection services)
+    {
+        services.AddSingleton<DeviceRepository>();
+        
+        services.AddSingleton<ParserService>();
+        services.AddSingleton<BackgroundParserService>();
+    }
+
+    private static void RegisterViewModels(IServiceCollection services)
+    {
+        services.AddSingleton<DevicesViewModel>();
+        services.AddSingleton<SettingsViewModel>();
+        
+        services.AddTransient<ResultsViewModel>();
+        services.AddTransient<AddDeviceViewModel>();
+        
+    }
+
+    private static void RegisterPages(IServiceCollection services)
+    {
+        services.AddSingleton<DevicesPage>();
+        services.AddSingleton<SettingsPage>();
+        services.AddSingleton<QuestionnairePage>();
+        
+
+        services.AddTransient<ResultsPage>();
+        services.AddTransient<AddDevicePage>();
+    }
 }
